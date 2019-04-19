5ffad122c139097f4e223a0bce2d3c3afa0a38bf
AhmedSherifTalaatYehia <ahmedsherif1591997@gmail.com>
2019-04-19 09:58:37 +0200

    remove all

--- modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .train.train import NerLearner
-from .data.bert_data import BertNerData
-from .models.bert_models import BertBiLSTMCRF
-
-
-__all__ = ["NerLearner", "BertNerData", "BertBiLSTMCRF"]
